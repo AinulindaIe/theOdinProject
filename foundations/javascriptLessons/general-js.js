8760f8e7282acd57a07725039d94611f184bc488
ainulindaie <pelle.galloe@gmail.com>
2022-11-14 11:56:31 +0100

    Provides intial js setup and examples
    Downloaded folder for javascript exercises from oden-repo
    since this is its own repository, I am not including it
    in this project. As a result my solution to the exercises
    might not show up in this repo either.
    
    -ainu

---
+++ foundations/javascriptLessons/general-js.js
@@ -0,0 +1,59 @@
+//"use strict"; //Good practice to pervent errors by assigning values to a variable before declaring it
+
+/* 
+let message;
+
+message = 'Hello';
+
+alert(message);
+
+// Or
+
+let msg = 'Hello';
+
+alert(msg);
+*/
+
+/* 
+//Bad practice!
+let user = 'John', age = 25, message = 'Hello';
+
+//Good practice!
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+// or
+let user = 'John',
+    age = 25,
+    message = 'Hello';
+
+// or
+let user = 'John'
+    , age = 25
+    , message = 'Hello';
+*/
+
+
+/*
+const myBirthday = '18.04.1900';
+
+const COLOR_RED = '#F00';
+
+let color = COLOR_RED;
+*/
+
+let admin;
+let adminName;
+
+adminName = 'John';
+admin = adminName;
+alert(admin);
+
+let homePlanet;
+
+homePlanet = 'Earth';
+
+let currentUser;
+
+
