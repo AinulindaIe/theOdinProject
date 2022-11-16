72269b22b5a92ed284bf261fa04d1497058aaa83
ainulindaie <pelle.galloe@gmail.com>
2022-11-16 19:17:08 +0100

    Adds two more brushes and polishes the structure of the js-file
    Working with different brushes and needs, I rearranged how
    some of the code was designed, to better fit the modularity
    of adding different brushes. Especially did i redesign the
    way eventlisteners are assigned to the squares, as the
    previous version was too taylor made to the binary brush
    listener.
    
    -ainu

--- foundations/odin-etch-a-sketch/script.js
+++ foundations/odin-etch-a-sketch/script.js
@@ -11,6 +11,7 @@
 
 */
 const GRIDWINDOW_SIZE = 720;
+const COLORS = ['#A0E4CB', '#59C1BD', '#0D4C92']
 // GRID setup
 function newGrid(units){
     const grid = document.querySelector('.grid-container');
@@ -27,6 +28,7 @@ function newGrid(units){
                 const square = document.createElement('div');
                 square.classList.add('grid-square');
                 square.style.cssText += `width: ${side}px; height: ${side}px;`
+                square.style.opacity = 0;
                 row.appendChild(square);
             }
             
@@ -46,33 +48,79 @@ function removeGrid() {
 }
 
 // Change brush-stroke
-function setBrush(brushFunction){
+function setBrush(brushFunction, eventKey){
     const gridSquares = Array.from(document.querySelectorAll('.grid-square'));
-    gridSquares.forEach(square => square.addEventListener('mouseenter', brushFunction));
+    gridSquares.forEach(square => square.addEventListener(eventKey, brushFunction));
 }
 
-function deselectBrush(brushFunction){
+function deselectBrush(brushFunction, eventKey){
     const gridSquares = Array.from(document.querySelectorAll('.grid-square'));
-    gridSquares.forEach(square => square.removeEventListener('mouseenter', brushFunction));
+    gridSquares.forEach(square => square.removeEventListener(eventKey, brushFunction));
 }
 
 // BRUSHES
 function binaryBrush(e) {
-    this.style.cssText += "background-color: black;";
+    this.style.opacity = 1;
 }
 
+function slowShadingBrush(e) {
+    const opacity = ((parseFloat(this.style.opacity) + 0.1) * 10) / 10;
+    
+    if (opacity < 1.1) {
+        this.style.opacity = opacity
+    }
+}
+
+function colorBrush(e) {
+     this.style.backgroundColor = COLORS[Math.floor(Math.random() * 3)];
+}
+
+
+// BRUSH setup
+
+function enableBinaryBrush() {
+    deselectBrush(currentBrush, currentEventKey);
+    setBrush(binaryBrush, 'mouseenter');
+    currentBrush = binaryBrush;
+    currentEventKey = 'mouseenter';
+}
+
+function enableShadingBrush() {
+    deselectBrush(currentBrush, currentEventKey);
+    setBrush(slowShadingBrush, 'mouseenter');
+    currentBrush = slowShadingBrush;
+    currentEventKey = 'mouseenter';
+
+}
+
+function enableColorBrush() {
+    deselectBrush(currentBrush, currentEventKey);
+    setBrush(colorBrush, 'mouseenter');
+    currentBrush = colorBrush;
+    currentEventKey = 'mouseenter';
+}
 
 //Select grid interaction with user
 function selectGrid(e) {
     const size = prompt("Choose grid-size (1-100)");
     removeGrid();
     newGrid(size);
-    setBrush(currentBrush);
+    setBrush(currentBrush, 'mouseenter');
 }
 // Initial configuration
 newGrid(16);
 let currentBrush = binaryBrush;
-setBrush(currentBrush);
+let currentEventKey = 'mouseenter'
+setBrush(currentBrush, currentEventKey);
 
 const promptBtn = document.querySelector('.prompt-btn');
 promptBtn.addEventListener('click', selectGrid);
+
+const binaryBrushBtn = document.querySelector('.binary');
+binaryBrushBtn.addEventListener('click', enableBinaryBrush);
+
+const shadeBrushBtn = document.querySelector('.shading');
+shadeBrushBtn.addEventListener('click', enableShadingBrush);
+
+const colorBrushBtn = document.querySelector('.color');
+colorBrushBtn.addEventListener('click', enableColorBrush);
